ae439d47b19a34bd58fd92a4db4f35e94ce8fe92
cd <azzamathar@hotmail.com>
2019-09-23 12:20:55 +0000

    adds hooks and reducers

---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,134 @@
+import { useEffect, useReducer } from "react";
+import axios from "axios";
+require("dotenv").config();
+
+const SET_DAY = "SET_DAY";
+const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+const SET_INTERVIEW = "SET_INTERVIEW";
+
+const updateSpots = function(state, decrease) {
+  return state.days.map(day => {
+    if (day.name !== state.day) {
+      return day;
+    }
+
+    return {
+      ...day,
+      spots: decrease ? day.spots-- : day.spots++
+    };
+  });
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case SET_DAY: {
+      return { ...state, day: action.day };
+    }
+    case SET_APPLICATION_DATA: {
+      return {
+        ...state,
+        days: action.days,
+        appointments: action.appointments,
+        interviewers: action.interviewers
+      };
+    }
+    case SET_INTERVIEW: {
+      const appointment = {
+        ...state.appointments[action.id],
+        interview: action.interview ? { ...action.interview } : null
+      };
+
+      const appointments = {
+        ...state.appointments,
+        [action.id]: appointment
+      };
+
+      let days = state.days;
+      
+      if (action.interview && !state.appointments[action.id].interview) {
+        days = updateSpots(state, true);
+      } else if (!action.interview && state.appointments[action.id].interview) {
+        days = updateSpots(state, false);
+      }
+
+      return { ...state, ...days, appointments };
+    }
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
+  }
+}
+
+export default function useApplicationData() {
+  const [state, dispatch] = useReducer(reducer, {
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
+
+  useEffect(() => {
+    const getDays = axios.get("/api/days");
+    const getAppointments = axios.get("/api/appointments");
+    const getInterviewers = axios.get("/api/interviewers");
+
+    Promise.all([getDays, getAppointments, getInterviewers])
+      .then(response => {
+        dispatch({
+          type: "SET_APPLICATION_DATA",
+          days: response[0].data,
+          appointments: response[1].data,
+          interviewers: response[2].data
+        });
+      })
+      .catch(error => {
+        throw error;
+      });
+
+    const webSocket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+
+    webSocket.onmessage = function(event) {
+      event = JSON.parse(event.data);
+
+      if (event.type === SET_INTERVIEW) {
+        dispatch({ ...event });
+      } else {
+        console.log(
+          "fix me:",
+          event
+        );
+      }
+    };
+  }, []);
+
+  const setDay = day => dispatch({ type: SET_DAY, day });
+
+  const bookInterview = function(id, interview) {
+    const putInterview = axios.put(`/api/appointments/${id}`, {
+      interview: interview
+    });
+
+    return Promise.all([putInterview])
+      .then(() => {
+        dispatch({ type: SET_INTERVIEW, id, interview });
+      })
+      .catch(error => {
+        throw error;
+      });
+  };
+
+  const cancelInterview = function(id) {
+    const deleteInterview = axios.delete(`/api/appointments/${id}`);
+
+    return Promise.all([deleteInterview])
+      .then(() => {
+        dispatch({ type: SET_INTERVIEW, id });
+      })
+      .catch(error => {
+        throw error;
+      });
+  };
+
+  return { state, setDay, bookInterview, cancelInterview };
+}
---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+export default function useVisualMode(initial){
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = function (mode, replace) {
+    setMode(mode);
+    if (replace) {
+      setHistory(prev => [mode, ...(prev.slice(1))]);
+    } else {
+      setHistory(prev => [mode, ...prev]);
+    }
+  }
+
+  const back = () => {
+    if (history.length > 1) {
+      setMode(history[1]);
+      setHistory(prev => prev.slice(1));
+    }
+  }
+
+  return {
+    mode,
+    transition,
+    back
+  };
+}
